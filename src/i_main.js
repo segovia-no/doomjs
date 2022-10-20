ee5f112cbd6d57615c5ce9af05b40258b7575a37
Diego Segovia <dsegoviaolivares@gmail.com>
2022-10-19 23:23:47 -0300

    Removed legacy js implementation

--- src/i_main.js
+++
@@ -1,6 +0,0 @@
-const D_DoomMain = require('./d_main').D_DoomMain
-
-global.myargs = process.argv
-
-//  init doom
-D_DoomMain()
--- src/v_video.js
+++
@@ -1,8 +0,0 @@
-function V_Init() {
-  //  "screen initialization" is handled by electron
-  console.log('V_Init: allocate screens. ignoring')
-}
-
-module.exports = {
-  V_Init
-}

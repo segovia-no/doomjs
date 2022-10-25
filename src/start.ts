14e22ac7adc1bfb5ea24a01b5efb7987954c4ec9
Diego Segovia <dsegoviaolivares@gmail.com>
2022-10-24 21:59:30 -0300

    Start file cleaning

--- src/start.ts
+++ src/start.ts
@@ -1,18 +1,5 @@
 import Game from './game'
 
-//program init
-// const wadLoader = new WADLoader('./DOOM.WAD')
-
-// wadLoader.loadWAD()
-
-// //new map
-// let map: Map = new Map('E1M1')
-// wadLoader.loadMapData(map)
-
-// map.printMapInfo()
-// map.printHeadVertexes(5)
-// map.printHeadLinedefs(5)
-
 const game: Game = new Game('./DOOM.WAD', 'E1M1')
 game.init()
 
